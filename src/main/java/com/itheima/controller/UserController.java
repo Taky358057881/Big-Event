ea9b7f8f5fbc7b0f632b250e223bac4a42e803a9
Taky <358057881@qq.com>
2024-07-12 16:03:26 +0800

    注册接口参数校验

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -3,6 +3,7 @@ package com.itheima.controller;
 import com.itheima.pojo.Result;
 import com.itheima.pojo.User;
 import com.itheima.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,16 +17,12 @@ public class UserController {
     private UserService userService;;
 
     @PostMapping("/register")
-    public Result register(String username, String password) {
-        //查询用户
-        User u = userService.findByUserName(username);
-        if(u == null){
-            //没有被占用
-            //注册
+    public Result<User> register(@Pattern(regexp = "^\\S{5,16}$") final String username, @Pattern(regexp = "^\\S{5,16}$") final String password){
+        final User existingUser = userService.findByUserName(username);
+        if(existingUser == null){
             userService.register(username,password);
             return Result.success();
         }else{
-            //占用
             return Result.error("用户名已被占用");
         }
     }
