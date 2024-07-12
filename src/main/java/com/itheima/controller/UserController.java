03cbfb8fbff4713bc265beef1c69a85333eef9d7
Taky <358057881@qq.com>
2024-07-12 17:20:00 +0800

    登录认证

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -3,14 +3,21 @@ package com.itheima.controller;
 import com.itheima.pojo.Result;
 import com.itheima.pojo.User;
 import com.itheima.service.UserService;
+import com.itheima.utils.JwtUtil;
+import com.itheima.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
@@ -25,6 +32,33 @@ public class UserController {
         }else{
             return Result.error("用户名已被占用");
         }
+    }
+
+    @PostMapping("/login")
+    public Result<String> login(@Pattern(regexp = "^\\S{5,16}$") final String username,@Pattern(regexp = "^\\S{5,16}$") final String password){
+        //final User existingUser = userService.findByUserName(username);
+        User loginUser = userService.findByUserName(username);
+
+        if(loginUser == null){
+            return Result.error("用户不存在");
+        }
+
+        if(Md5Util.getMD5String(password).equals(loginUser.getPassword())){
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", loginUser.getUsername());
+            claims.put("username", loginUser.getUsername());
+            String token = JwtUtil.genToken(claims);
+            return Result.success(token);
+        }
+
+        return Result.error("密码错误");
+
+//        if(Md5Util.getMD5String(password).equals(existingUser.getPassword())){
+//            final Map<String, Object> map = Map.of("id",existingUser.getId(),
+//                    "username", existingUser.getUsername());
+//            final String token = JwtUtil.genToken(map);
+//        }
+
     }
 
 
