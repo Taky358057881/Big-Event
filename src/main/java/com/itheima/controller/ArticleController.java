a8f67f68aa19469edb7b2022b15ef9e24428635d
Taky <358057881@qq.com>
2024-07-15 18:15:17 +0800

    文章分类

--- src/main/java/com/itheima/controller/ArticleController.java
+++ src/main/java/com/itheima/controller/ArticleController.java
@@ -9,9 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/article")
 public class ArticleController {
 
-    @GetMapping("/list")
-    public Result<String> list(){
-        return Result.success("所有文中数据......");
-    }
 
 }
